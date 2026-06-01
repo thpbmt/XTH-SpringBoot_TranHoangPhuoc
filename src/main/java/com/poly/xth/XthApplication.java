acf7b51622f94f0b7c5a1a05ce3de4bed01c1505
thpbmt <you@example.com>
2026-06-01 21:07:41 +0700

    first commit

---
+++ src/main/java/com/poly/xth/XthApplication.java
@@ -0,0 +1,13 @@
+package com.poly.xth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/xth/XthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.xth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
